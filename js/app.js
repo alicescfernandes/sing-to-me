50bef63bd6a41a1e7fadbc4726f5c06380e0b407
Alice Fernandes <15971182+alicescfernandes@users.noreply.github.com>
2021-03-08 21:46:51 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@
     }
 
     //Variables
-    var base = "https://alicescfernandes-projects.000webhostapp.com/singtome/" //window.location.origin + window.location.pathname + "php/";
+    var base = "https://alicescfernandes.000webhostapp.com/singtome/" //window.location.origin + window.location.pathname + "php/";
     var audioService = "deezer"
     var height = 0;
 
